fd91446451d08a752c0ca1234af63cddea1a0ee4
Adam Minkus <adam.minkus@gmail..com>
2019-07-17 20:39:19 -0700

    update dashboard functionality

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ let axios = require("axios");
 
 
 var app = express();
-var PORT = process.env.PORT || 8079;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -30,15 +30,12 @@ else {
     host: "localhost",
     port: 3306,
     user: "root",
-<<<<<<< HEAD
     password: "password",
-=======
-    password: "nst032596",
->>>>>>> nt
     database: "practice_db"
   });
 }
 //****************************************/
+
 
 app.get('/login'), (req, res) => {
   res.render('login')
@@ -80,7 +77,10 @@ app.get('/login/fail', (req, res) => {
 })
 
 app.get('/dashboard', (req, res) => {
-  res.render('dashboard')
+  db.exerciseInfo.findAll({}).then(function (data) {
+    res.render('dashboard', { data: data })
+  })
+
 })
 
 app.get('/weight', (req, res) => {
@@ -150,218 +150,182 @@ app.post('/survey', (req, res) => {
   updateUser(req.body, res)
 })
 
-app.get("/dashboard", (req, res) => {
-  res.render('dashboard')
-});
-
-app.post("/dashboard", (req, res) => {
+app.get("/api/dashboard/:condition/:level", (req, res) => {
   // console.log(req.body);
-  db.userHistory.create({
-    exerciseType: req.body.exerciseType,
-    exerciseIntensity: req.body.exerciseIntensity
-    //maybe more data for graphing later
-  }).then(function (results) {
-
-    var arr = [{
-      exerciseType: 'Legs',
-      intensity: 10,
-      image: 'http://image.png',
-      description: 'an exercise 12334'
-    }, {
-      exerciseType: 'Legs',
-      intensity: 10,
-      image: 'http://image.png',
-      description: 'an exercise 4567'
-    }, {
-      exerciseType: 'Legs',
-      intensity: 1,
-      image: 'http://image.png',
-      description: 'an exercise 567789'
-    }, {
-      exerciseType: 'Legs',
-      intensity: 2,
-      image: 'http://image.png',
-      description: 'an exercise 567789'
-    }, {
-      exerciseType: 'Arms',
-      intensity: 3,
-      image: 'http://image.png',
-      description: 'an exercise 567789'
-    }, {
-      exerciseType: 'Arms',
-      intensity: 5,
-      image: 'http://image.png',
-      description: 'an exercise 567789'
-    }
-    ];
-    var newArr = [];
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i].exerciseType == req.body.exerciseType && arr[i].intensity == req.body.exerciseIntensity) {
-        newArr.push(arr[i])
-      }
-    }
-
-
-
-    res.json(newArr)
-  })
-})
-
-
-// Function to make sure user has updated today, to take to weight entery screen (via login post [nested])
-let checkDate = (x, res) => {
-  db.users.findAll({
-    attributes: ['userBornToday', 'updatedAt'],
+  console.log(req.params)
+  db.exerciseInfo.findAll({
     where: {
-      email: x
+      muscle_group: req.params.condition,
+      level: req.params.level
     }
-  }).then(response => {
-    let date = new Date()
-    let userDate = response[0].dataValues.updatedAt;
-    if (userDate.setHours(0, 0, 0, 0) != date.setHours(0, 0, 0, 0)) {
-      res.redirect('/weight')
-    }
-    else {
-      res.redirect('/dashboard')
-    }
+  }).then((results) => {
+    console.log(results)
+    res.json(results)
   })
-}
+});
+  app.post("/dashboard", (req, res) => {
 
+    db.userHistory.create({
+      exerciseType: req.body.exerciseType,
+      exerciseIntensity: req.body.exerciseIntensity
+      //maybe more data for graphing later
+    }).then(function (results) {
 
-//function that is called to make sure login credentials are correct and take user to correct screen (via login post)
+      res.json(results)
+    });
+  });
 
-let authenticateUser = (x, a) => {
-
-  db.users.findAll({
-    where: {
-      email: x.email
-    }
-  }).then((response) => {
-    if (typeof response[0] === "undefined") { a.redirect('login/fail') }
-    else {
-      if (x.password === response[0].password && response[0].userBorn == 0) {
-        a.redirect('/survey')
+  // Function to make sure user has updated today, to take to weight entery screen (via login post [nested])
+  let checkDate = (x, res) => {
+    db.users.findAll({
+      attributes: ['userBornToday', 'updatedAt'],
+      where: {
+        email: x
       }
-      else if (x.password === response[0].password && response[0].userBorn === 1) {
-        checkDate(x.email, a);
+    }).then(response => {
+      let date = new Date()
+      let userDate = response[0].dataValues.updatedAt;
+      if (userDate.setHours(0, 0, 0, 0) != date.setHours(0, 0, 0, 0)) {
+        res.redirect('/weight')
       }
       else {
-        a.redirect('login/fail')
+        res.redirect('/dashboard')
       }
-    }
-  })
-}
+    })
+  }
 
-//function that will execute to make sure user login is unique(via register post route)
-let checkEmail = (a, b, c) => {
-  let empty = []
-  db.users.findAll({
-    attributes: ['email']
-  }).then(function (response) {
-    for (var i = 0; i < response.length; i++) {
-      empty.push(response[i].dataValues.email)
-    }
-    console.log(empty)
-    if (empty.indexOf(a) === -1) {
-      db.users.create({
-        name: b.firstName + b.lastName,
-        email: b.email,
-        password: b.password,
-        phoneNumber: b.phone,
-      }).then(function (response) {
-        console.log('look')
-        c.redirect('/login')
-      })
-    }
-    else {
-      console.log('fail')
-      c.redirect('/failed')
-    }
-  })
-}
 
-// Function that will execute once user presses submit on survey ( via post route of survey)
-let updateUser = (x, res) => {
-  db.users.update({
-    age: x.age,
-    gender: x.gender,
-    height: x.height,
-    weight: x.weight / 2.2,
-    weightGoal: x.goal / 2.2,
-    userBorn: 1
-  },
-    { where: { email: x.username } }
-  ).then(function (data) {
-    getCals(x.username)
-    console.log('checked')
-    res.redirect('dashboard')
+  //function that is called to make sure login credentials are correct and take user to correct screen (via login post)
 
-  })
-}
+  let authenticateUser = (x, a) => {
 
-//function to generate daily calorie goal based on user weight and height and gender and age
-let getCals = (x) => {
-  db.users.findAll({
-    attributes: ['age', 'height', 'weight', 'gender'],
-    where: {
-      email: x
-    }
-  }).then(function (response) {
-    console.log(response[0].dataValues)
-    let fatPercentage = Number(response[0].dataValues.height / (response[0].dataValues.weight * 3.68))
-    let fatFreeMass = (response[0].dataValues.weight - (response[0].dataValues.weight * fatPercentage))
-    let calsTemp = (500 + (22 * fatFreeMass))
-    if (response[0].dataValues.gender == 'male') {
-      calsTemp += 105
-    }
-    if (response[0].dataValues.age < 35) {
-      calsTemp += 135
-    }
-    let cals = Math.floor(calsTemp)
-    if (cals > 2550) {
-      cals = 2475
-    }
-    db.users.update({
-      calories: cals
-    },
-      {
-        where: {
-          email: x
+    db.users.findAll({
+      where: {
+        email: x.email
+      }
+    }).then((response) => {
+      if (typeof response[0] === "undefined") { a.redirect('login/fail') }
+      else {
+        if (x.password === response[0].password && response[0].userBorn == 0) {
+          a.redirect('/survey')
+        }
+        else if (x.password === response[0].password && response[0].userBorn === 1) {
+          checkDate(x.email, a);
+        }
+        else {
+          a.redirect('login/fail')
         }
       }
-    ).then(function (response) {
-      console.log('worked first time')
     })
+  }
 
-  })
-}
+  //function that will execute to make sure user login is unique(via register post route)
+  let checkEmail = (a, b, c) => {
+    let empty = []
+    db.users.findAll({
+      attributes: ['email']
+    }).then(function (response) {
+      for (var i = 0; i < response.length; i++) {
+        empty.push(response[i].dataValues.email)
+      }
+      console.log(empty)
+      if (empty.indexOf(a) === -1) {
+        db.users.create({
+          name: b.firstName + b.lastName,
+          email: b.email,
+          password: b.password,
+          phoneNumber: b.phone,
+        }).then(function (response) {
+          console.log('look')
+          c.redirect('/login')
+        })
+      }
+      else {
+        console.log('fail')
+        c.redirect('/failed')
+      }
+    })
+  }
+
+  // Function that will execute once user presses submit on survey ( via post route of survey)
+  let updateUser = (x, res) => {
+    db.users.update({
+      age: x.age,
+      gender: x.gender,
+      height: x.height,
+      weight: x.weight / 2.2,
+      weightGoal: x.goal / 2.2,
+      userBorn: 1
+    },
+      { where: { email: x.username } }
+    ).then(function (data) {
+      getCals(x.username)
+      console.log('checked')
+      res.redirect('dashboard')
+
+    })
+  }
+
+  //function to generate daily calorie goal based on user weight and height and gender and age
+  let getCals = (x) => {
+    db.users.findAll({
+      attributes: ['age', 'height', 'weight', 'gender'],
+      where: {
+        email: x
+      }
+    }).then(function (response) {
+      console.log(response[0].dataValues)
+      let fatPercentage = Number(response[0].dataValues.height / (response[0].dataValues.weight * 3.68))
+      let fatFreeMass = (response[0].dataValues.weight - (response[0].dataValues.weight * fatPercentage))
+      let calsTemp = (500 + (22 * fatFreeMass))
+      if (response[0].dataValues.gender == 'male') {
+        calsTemp += 105
+      }
+      if (response[0].dataValues.age < 35) {
+        calsTemp += 135
+      }
+      let cals = Math.floor(calsTemp)
+      if (cals > 2550) {
+        cals = 2475
+      }
+      db.users.update({
+        calories: cals
+      },
+        {
+          where: {
+            email: x
+          }
+        }
+      ).then(function (response) {
+        console.log('worked first time')
+      })
+
+    })
+  }
 
 
-//practice food parser request
-let apiCall = () => {
-  let url = "https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=red%20apple&app_id=153d107f&app_key=b7785b3de6ea8b46bb8efa79c39c4166"
-  axios.get(url).then(function (response) {
-    console.log(response.data.hints[0].food.nutrients.ENERC_KCAL)
-  })
-}
-apiCall()
+  //practice food parser request
+  let apiCall = () => {
+    let url = "https://api.edamam.com/api/food-database/parser?nutrition-type=logging&ingr=red%20apple&app_id=153d107f&app_key=b7785b3de6ea8b46bb8efa79c39c4166"
+    axios.get(url).then(function (response) {
+      console.log(response.data.hints[0].food.nutrients.ENERC_KCAL)
+    })
+  }
+  apiCall()
 
-let apicall2 = () => {
-  let url = "http://api.edamam.com/auto-complete?q=pe&limit=10&app_id=153d107f&app_key=b7785b3de6ea8b46bb8efa79c39c4166"
-  axios.get(url).then(function (response) {
-    console.log(response.data)
-  })
-}
-apicall2()
-db.sequelize.sync().then(function () {
-  app.listen(PORT, function () {
-    console.log("App listening on PORT " + PORT);
+  let apicall2 = () => {
+    let url = "http://api.edamam.com/auto-complete?q=pe&limit=10&app_id=153d107f&app_key=b7785b3de6ea8b46bb8efa79c39c4166"
+    axios.get(url).then(function (response) {
+      console.log(response.data)
+    })
+  }
+  apicall2()
+
+
+  db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+      console.log("App listening on PORT " + PORT);
+    });
   });
-});
-
-
-
-
-
 
 
---
+++ sequelizeSeeds.js
@@ -0,0 +1,15 @@
+var db = require('./models');
+db.exerciseInfo.create({
+    exercise_name:'jumping jacks',
+    description:'they are jumping jacks',
+    level:1,
+    muscle_group:'Arms',
+    link:'fakelink'
+})
+db.exerciseInfo.create({
+    exercise_name:'bicep curls',
+    description:'they are jumping jacks',
+    level:1,
+    muscle_group:'Arms',
+    link:'fakelink'
+})
