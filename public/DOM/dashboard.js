0241e52a04f51b452b1a89405a47333764827cdc
Scott Willis <scott.willis.wsu@gmail.com>
2019-07-18 13:32:21 -0700

    test

--- public/DOM/dashboard.js
+++ public/DOM/dashboard.js
@@ -17,21 +17,51 @@ $("#intensity a").on("click", function () {
 
 $("#daily-workout").on("click", function (event) {
   event.preventDefault();
+  $(".card").empty();
   // console.log(workoutData);
 
   $.get('/api/dashboard/' + workoutData.exerciseType + '/' + workoutData.exerciseIntensity)
     .then(function (data) {
       console.log(data)
-    })
+      for (let i = 0; i < data.length; i++) {
+        const element = data[i];
+        console.log(element);
 
-  //stores user input into userHistory table
-  $.post("/dashboard", workoutData)
-    .then(function (data) {
-      console.log(data)
+        let newCard = $('<div>').addClass("card");
+        newCard.attr("width", "18rem");
+
+        let newImage = $("<img>").addClass("card-img-top");
+        // newImage.attr("src", element.image_link)
+        newImage.attr("alt", "Card image cap");
+        newCard.append(newImage)
+
+        let newDivCard = $("<div>").addClass("card-body");
+        newCard.append(newDivCard);
+
+        let newCardTitle = $("<h5>").addClass("card-title");
+        newCardTitle.text(element.exercise_name)
+        newDivCard.append(newCardTitle);
+
+        let newCardText = $("<p>").addClass("card-text");
+        newCardText.text(element.description)
+        newCardTitle.append(newCardText);
+
+        let newUl = $("<ul>").addClass("list-group list-group-flush");
+        newDivCard.append(newUl);
+
+        let newLi1 = $("<li>").addClass("list-group-item");
+        newLi1.text(element.muscle_group);
+        newUl.append(newLi1);
+
+        let newLi2 = $("<li>").addClass("list-group-item");
+        newLi2.text(element.level);
+        newUl.append(newLi2);
+
+        let newLi3 = $("<li>").addClass("list-group-item");
+        newLi3.attr("href", element.video_link);
+        newUl.append(newLi3);
+
+        $("#cardHolder").append(newCard);
+      }
     })
 })
-
-
-//populate username on navbar
-var userFirstName = localStorage.getItem("userFirstName");
-$("#unique-user").text(userFirstName);
