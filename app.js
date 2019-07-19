793b402bce113b3f17af6efd890b27c8b34c5eac
Scott Willis <scott.willis.wsu@gmail.com>
2019-07-19 09:03:26 -0700

    test

--- app.js
+++ app.js
@@ -22,19 +22,7 @@ app.set("view engine", "handlebars");
 
 var db = require("./models");
 
-// var connection;
-// if (process.env.JAWSDB_URL) {
-//   connection = mysql.createConnection(process.env.JAWSDB_URL)
-// }
-// else {
-//   connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "practice_db"
-//   });
-// }
+
 //****************************************/
 
 
